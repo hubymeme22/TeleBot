70e32a31ce948f34be65c59a58d72b65dbe7e068
hubymeme22 <huby.new1112@gmail.com>
2022-07-23 19:59:39 +0800

    Ban word command working    Future works:    - penalty command will be added soon    - delete bot will be added    - checker if the bot can be used for this command (group privacy disabled)

--- BotMap/functionalities/ban.js
+++ BotMap/functionalities/ban.js
@@ -11,7 +11,16 @@ function banWord(bot_instance, msg, match) {
 	const word = match[1];
 	console.log(`banned word: ${word}`);
 
-	// soon here...
+	// send notification to client:
+	bot_instance.sendMessage(msg.chat.id, `word *${word}* added to banned word list`);
+
+	// callback added for listening incoming messages
+	bot_instance.on('message', (message) => {
+		console.log('Bot message logging is called');
+		if (message.text.includes(word) && message.chat.id == msg.chat.id)
+			bot_instance.sendMessage(message.chat.id, 'That word is banned! warning is given to you');
+	});
+
 }
 
 module.exports = {
