11f41d112942ea219f13d6cb62b9de7d70ef9b5f
hubymeme22 <huby.new1112@gmail.com>
2022-07-25 16:53:05 +0800

    Addition and changes to ban    - Design of banning algorithm got changed, so we can unban words    - Mapping of chat id and words to be banned are is used    - unban functionality added

--- BotMap/functionalities/ban.js
+++ BotMap/functionalities/ban.js
@@ -7,22 +7,53 @@
 
 // By: Hubert F. Espinola I
 
+const id_word_map = {
+	// chat_id : [banned_words]
+};
+
 function banWord(bot_instance, msg, match) {
 	const word = match[1];
-	console.log(`banned word: ${word}`);
 
-	// send notification to client:
-	bot_instance.sendMessage(msg.chat.id, `word *${word}* added to banned word list`);
+	// check if this chat room is new
+	if (!id_word_map.hasOwnProperty(msg.chat.id)) {
+		// add listener to this chatroom
+		id_word_map[msg.chat.id] = [];
+		bot_instance.on('message', (cli_msg) => {
+			for (let i = 0; i < id_word_map[msg.chat.id].length; i++) {
+				if (cli_msg.text.includes(id_word_map[msg.chat.id][i])) {
+					bot_instance.sendMessage(cli_msg.chat.id, 'A banned word is used, warning is given to the user.');
+					break;
+				}
+			}
+		});
+	}
 
-	// callback added for listening incoming messages
-	bot_instance.on('message', (message) => {
-		console.log('Bot message logging is called');
-		if (message.text.includes(word) && message.chat.id == msg.chat.id)
-			bot_instance.sendMessage(message.chat.id, 'That word is banned! warning is given to you');
-	});
+	// add the word to chatroom's banned wordlist
+	if (id_word_map[msg.chat.id].indexOf(word) == -1) {
+		id_word_map[msg.chat.id].push(word);
+		bot_instance.sendMessage(msg.chat.id, 'Word is added to banned word list.');
+	} else {
+		bot_instance.sendMessage(msg.chat.id, 'The word you are trying to add is already on the list.')
+	}
+}
 
+function unbanWord(bot_instance, msg, match) {
+	const word = match[1];
+	if (!id_word_map.hasOwnProperty(msg.chat.id)) {
+		bot_instance.sendMessage(msg.chat.id, 'You have not banned a single word yet');
+		return;
+	}
+
+	if (id_word_map[msg.chat.id].indexOf(word) == -1) {
+		bot_instance.sendMessage(msg.chat.id, 'The word you are trying to ban does not exist in the wordlist');
+		return;
+	}
+
+	id_word_map[msg.chat.id].pop(word);
+	bot_instance.sendMessage(msg.chat.id, 'The word is removed from the list');
 }
 
 module.exports = {
-	'ban' : banWord
+	'ban' : banWord,
+	'unban' : unbanWord
 }
